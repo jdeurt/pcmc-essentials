5b68b80eefce0b8f45bc5810022b7e5774622dbd
Juan de Urtubey <jdeurt@gmail.com>
2020-04-10 15:59:18 -0500

    Initial commit

---
+++ src/com/sharky/pcmc/PCMCEssentials.java
@@ -0,0 +1,15 @@
+package com.sharky.pcmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PCMCEssentials extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
