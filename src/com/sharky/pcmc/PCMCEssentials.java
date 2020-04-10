e8509670a98d1b3df45a4ac6391583d4e6f29637
Juan de Urtubey <jdeurt@gmail.com>
2020-04-10 16:16:16 -0500

    Added command templates

--- src/com/sharky/pcmc/PCMCEssentials.java
+++ src/com/sharky/pcmc/PCMCEssentials.java
@@ -1,15 +1,25 @@
 package com.sharky.pcmc;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
 
-public class PCMCEssentials extends JavaPlugin {
+public class PCMCEssentials extends JavaPlugin implements Listener {
+	FileConfiguration config = getConfig();
+	
 	@Override
 	public void onEnable() {
+		config.addDefault("allow-quad-reselect", false);
+		config.options().copyDefaults(true);
+		saveConfig();
 		
+		getServer().getPluginManager().registerEvents(this, this);
+		
+		getLogger().info("Enabled PCMC Essentials");
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		getLogger().info("Disabled PCMC Essentials");
 	}
 }
---
+++ src/com/sharky/pcmc/commands/CommandQuad.java
@@ -0,0 +1,12 @@
+package com.sharky.pcmc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandQuad implements CommandExecutor {
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		return false;
+	}
+}
