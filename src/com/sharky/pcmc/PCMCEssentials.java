53851f4e247c77c5be19c4102895bc06cc1fa127
Juan de Urtubey <jdeurt@gmail.com>
2020-04-10 16:40:25 -0500

    Created skeleton for quad command

--- src/com/sharky/pcmc/PCMCEssentials.java
+++ src/com/sharky/pcmc/PCMCEssentials.java
@@ -1,6 +1,9 @@
 package com.sharky.pcmc;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sharky.pcmc.commands.*;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 
@@ -9,11 +12,17 @@ public class PCMCEssentials extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
+		// Config stuff
 		config.addDefault("allow-quad-reselect", false);
 		config.options().copyDefaults(true);
 		saveConfig();
 		
+		// Register listeners
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		// Register commands
+		this.getCommand("quad").setExecutor(new CommandQuad(this));
+		this.getCommand("link").setExecutor(new CommandLink());
 		
 		getLogger().info("Enabled PCMC Essentials");
 	}
--- src/com/sharky/pcmc/commands/CommandQuad.java
+++ src/com/sharky/pcmc/commands/CommandQuad.java
@@ -1,12 +1,32 @@
 package com.sharky.pcmc.commands;
 
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandQuad implements CommandExecutor {
+	private JavaPlugin plugin;
+	
+	public CommandQuad(JavaPlugin plugin) {
+		this.plugin = plugin;
+	}
+	
+	private void setPlayerNameColor(Player player, String color) {
+		String oldName = player.getName();
+		Server server = this.plugin.getServer();
+		
+		server.dispatchCommand(server.getConsoleSender(), "nick " + oldName + " " + color + oldName);
+	}
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		return false;
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+		}
+		
+		return true;
 	}
 }
